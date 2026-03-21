8e2c35c7cefef3c024f0024180bf64cb1ade6b15
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-21 09:50:58 +0200

    Change names varchar to 50 and title to 100 from 255

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR ( 255 ) NOT NULL,
-    last_name VARCHAR ( 255 ) NOT NULL,
+    first_name VARCHAR ( 50 ) NOT NULL,
+    last_name VARCHAR ( 50 ) NOT NULL,
     email_address VARCHAR ( 255 ) UNIQUE NOT NULL,
     password_hash VARCHAR ( 255 ),
     membership_status BOOLEAN NOT NULL DEFAULT FALSE
@@ -13,7 +13,7 @@ CREATE TABLE users (
 CREATE TABLE messages (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER NOT NULL,
-    title VARCHAR ( 255 ) NOT NULL,
+    title VARCHAR ( 100 ) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     message_text TEXT NOT NULL,
     CONSTRAINT fk_users_cascade
