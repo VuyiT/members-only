ba728817ff21761a6c8906be477c7ea653676508
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-23 10:26:29 +0200

    Fix view registering syntax

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("node:path");
 const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
