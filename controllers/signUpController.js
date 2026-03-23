3eb8c01b75e51724efba150b990a8088539d724f
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-23 11:46:19 +0200

    Add a CreateUserPost array

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,5 +1,8 @@
 const db = require("../db/queries");
 const { CustomFailedToLoadError } = require("../errors/CustomErrors");
+const { validationResult, matchedData } = require("express-validator");
+const { validateUser } = require("../lib/validator");
+const bcrypt = require("bcryptjs");
 
 async function getSignUp(req, res) {
     try {
@@ -11,7 +14,28 @@ async function getSignUp(req, res) {
     }
 }
 
+const createUserPost = [
+    validateUser,
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("sign-up-form", {
+                title: "Sign Up",
+                errors: errors.array(),
+            });
+        }
+        try {
+            const { firstName, lastName, email, password } = matchedData(req);
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await db.insertUser(firstName, lastName, email, hashedPassword);
+            res.redirect("/");
+        } catch (err) {
+            next(err);
+        }
+    }
+];
 
 module.exports = {
     getSignUp,
+    createUserPost,
 }
