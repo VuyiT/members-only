8c2208587c1074cf3289302a1d1c71b66630d22f
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-23 10:29:04 +0200

    Register signUpRouter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const signUpRouter = require("./routes/signUpRouter");
 const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
@@ -9,6 +10,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/sign-up", signUpRouter);
 app.get("/", (req, res) => res.send("Members Only"));
 
 
