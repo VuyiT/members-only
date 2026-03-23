afcbb9844753f5e5302ec6aad1351b7411f1e62f
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-23 10:40:47 +0200

    Fix error handling in getSignUp

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,14 +1,13 @@
 const db = require("../db/queries");
 const { CustomFailedToLoadError } = require("../errors/CustomErrors");
 
-async function getSignUp(req, res, next) {
+async function getSignUp(req, res) {
     try {
         res.render("sign-up-form", {
             title: "Sign Up"
         });
     } catch (err) {
-        const error = new CustomFailedToLoadError("Signup page failed to load.");
-        return next(error);
+        throw new CustomFailedToLoadError("Signup page failed to load.");
     }
 }
 
