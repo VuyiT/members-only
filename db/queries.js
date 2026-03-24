7ac3edd01e4759f0dbcdd6140cc629fb55a04da4
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-24 11:38:39 +0200

    Create an updateMembershipStatus query

--- db/queries.js
+++ db/queries.js
@@ -4,6 +4,11 @@ async function insertUser(firstName, lastName, emailAddress, password) {
     await pool.query('INSERT INTO users (first_name, last_name, email_address, password_hash) VALUES ($1, $2, $3, $4)', [firstName, lastName, emailAddress, password]);
 }
 
+async function updateMembershipStatus(id) {
+    await pool.query("UPDATE users SET membership_status = TRUE WHERE id = $1", [id]);
+}
+
 module.exports = {
     insertUser,
+    updateMembershipStatus,
 }
