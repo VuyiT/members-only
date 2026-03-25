585fe0095733aaa45f2e156dd4fb197565e9bd28
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-25 07:48:41 +0200

    Register joinClubRouter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const signUpRouter = require("./routes/signUpRouter");
+const joinClubRouter = require("./routes/joinClubRouter");
 const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/join-club", joinClubRouter);
 app.use("/sign-up", signUpRouter);
 app.get("/", (req, res) => res.send("Members Only"));
 
