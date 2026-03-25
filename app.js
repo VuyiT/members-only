d64ce8d0249768e934580ab574656cfa3b67c938
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-25 13:41:39 +0200

    Register passport and session

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
 const app = express();
 const path = require("node:path");
 const signUpRouter = require("./routes/signUpRouter");
 const joinClubRouter = require("./routes/joinClubRouter");
+require("./config/passport");
 const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+     }
+}));
+app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/join-club", joinClubRouter);
