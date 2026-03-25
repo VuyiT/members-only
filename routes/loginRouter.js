88c6da0816217e1deba77d5c6114d978ef371d89
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-25 16:27:48 +0200

    Use passport authentication during login

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,14 @@
 const { Router } = require("express");
 const loginRouter = Router();
-const loginController = require("../controllers/loginController")
+const loginController = require("../controllers/loginController");
+const passport = require("passport");
+require("../config/passport");
 
 loginRouter.get("/", loginController.getLogin);
+loginRouter.post("/", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+})
+);
 
 module.exports = loginRouter;
