72aeb4b74cc29f367e0eb0a5092eb6a5b252d7c2
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-26 18:46:59 +0200

    Fix getMessages to getAllMessagesByUsers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,15 +2,13 @@ const db = require("../db/queries");
 const { CustomNotFoundError } = require("../errors/CustomErrors");
 
 async function getMessages(req, res) {
-    const messages = await db.getAllMessages();
-    const user = req.user;
+    const messages = await db.getAllMessagesByUsers();
     if (!messages) {
         throw new CustomNotFoundError("Messages not found");
     }
     res.render("index", {
         title: "Member Messages",
         messages,
-        user,
     });
 }
 
