d9fd2fbcd5d479d66e0ffabb9c6518ae15f78969
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-26 17:28:42 +0200

    Create CustomNotFoundError class

--- errors/CustomErrors.js
+++ errors/CustomErrors.js
@@ -5,7 +5,15 @@ class CustomFailedToLoadError extends Error {
         this.name = "FailedToLoadError";
     }
 }
+class CustomNotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = 404;
+        this.name = "NotFoundError";
+    }
+}
 
 module.exports = {
     CustomFailedToLoadError,
+    CustomNotFoundError
 };
