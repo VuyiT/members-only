b0cfefc3811a528113e52a75ccc8730558981532
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-26 15:24:38 +0200

    Create createNewMessagePost array to add a new message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,7 @@
 const db = require("../db/queries");
 const { CustomFailedToLoadError } = require("../errors/CustomErrors");
+const { validateMessage } = require("../lib/messageValidation");
+const { validationResult, matchedData } = require("express-validator");
 
 async function getNewMessage(req, res) {
     try {
@@ -11,6 +13,28 @@ async function getNewMessage(req, res) {
         }
 }
 
+const createNewMessagePost = [
+    validateMessage,
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("new-message-form", {
+                title: "Create New Message",
+                errors: errors.array(),
+            });
+        }
+        try {
+            const { id } = req.user;
+            const { title, messageText } = matchedData(req);
+            await db.insertMessage(id, title, messageText);
+            res.redirect("/");
+        } catch (err) {
+            next(err);
+        }
+    }
+];
+
 module.exports = {
     getNewMessage,
+    createNewMessagePost,
 }
