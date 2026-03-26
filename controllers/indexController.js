f2a9bd6a9475c0a8fa14e414303d4a89d4d72423
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-26 17:30:14 +0200

    Create getMessages function

---
+++ controllers/indexController.js
@@ -0,0 +1,19 @@
+const db = require("../db/queries");
+const { CustomNotFoundError } = require("../errors/CustomErrors");
+
+async function getMessages(req, res) {
+    const messages = await db.getAllMessages();
+    const user = req.user;
+    if (!messages) {
+        throw new CustomNotFoundError("Messages not found");
+    }
+    res.render("index", {
+        title: "Member Messages",
+        messages,
+        user,
+    });
+}
+
+module.exports = {
+    getMessages,
+}
