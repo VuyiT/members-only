35e3ebe1f2fe5a5685d37c6b07fcd6e5ee8a5d05
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-27 19:45:09 +0200

    Use getAllMessages query to get messages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,7 @@ const db = require("../db/queries");
 const { CustomNotFoundError } = require("../errors/CustomErrors");
 
 async function getMessages(req, res) {
-    const messages = await db.getAllMessagesByMembers();
+    const messages = await db.getAllMessages();
     if (!messages) {
         throw new CustomNotFoundError("Messages not found");
     }
