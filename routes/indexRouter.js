2e110d5127b063f99a2e079e8bfdcdacbf41498b
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-03-28 12:59:19 +0200

    Create deleteMessage route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,5 +3,6 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 indexRouter.get("/", indexController.getMessages);
+indexRouter.post("/:messageId/delete", indexController.deleteMessage);
 
 module.exports = indexRouter;
